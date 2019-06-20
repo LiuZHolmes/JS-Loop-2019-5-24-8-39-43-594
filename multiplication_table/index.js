5de33918d39aaf10b788ebfa85481af5d258a168
retr0 <395637649@qq.com>
2019-06-20 11:35:22 +0800

    fixing bug

--- multiplication_table/index.js
+++ multiplication_table/index.js
@@ -2,9 +2,10 @@ for (let i = 1;i <= 9; i++) {
 	var str = "";
 	for (let j = 1; j <= i; j++) {
 		if (j == 1) {
-		str = str + (i + "*" + j + "=" + (i*j));
+			str = str + (i + "*" + j + "=" + (i*j));
+		}
+		else
+			str = str + " " + (i + "*" + j + "=" + (i*j));
 	}
-	else
-		str = str + " " + (i + "*" + j + "=" + (i*j));
 	console.log(str);
 }
